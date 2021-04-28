a7983c409394a982a63c5860549b63f5d5519eb5
4Denthusiast <25589515+4Denthusiast@users.noreply.github.com>
2021-04-28 03:18:53 +0100

    Add empty window to try out GTK

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,16 @@
 module Main where
 
+import Control.Monad.Trans
+import Graphics.UI.Gtk
+
 import Lib
 
 main :: IO ()
-main = someFunc
+main = do
+    initGUI
+    window <- windowNew
+    windowSetTitle window "Hysteria Shooter"
+    windowSetDefaultSize window 512 512
+    on window deleteEvent (liftIO mainQuit >> return False)
+    widgetShowAll window
+    mainGUI
